3b3be7e0f1a25213e35935d35694831c1668f2de
jsweazy <jswaze@gmail.com>
2014-04-07 23:11:42 -0400

    Update coding style

--- app/server.js
+++ app/server.js
@@ -1,29 +1,30 @@
-var _ = require('underscore'),
-    config = require('./config.js'),
-    routes = require('./routes'),
-    engines = require('consolidate'),
-    express = require('express'),
+var _ = require( 'underscore' ),
+    config = require( './config.js' ),
+    routes = require( './routes' ),
+    engines = require( 'consolidate' ),
+    express = require( 'express' ),
     app = express();
 
 // Setup server
-app.engine('html', engines.underscore);
-app.set('view engine', 'html');
-app.use(app.router);
-app.use(express.static('/public'));
-app.use(express.compress());
-app.use(express.urlencoded());
+app.engine( 'html', engines.underscore );
+app.set( 'view engine', 'html' );
+app.use( app.router );
+app.use( express.static('/public') );
+app.use( express.compress() );
+app.use( express.urlencoded() );
 
 if ( config.env !== 'prod' ) {
-    app.use(express.logger('dev'));
+    app.use( express.logger('dev') );
 }
 
-app.get('/', routes.index);
+// Routes
+app.get( '/', routes.index );
 
-app.listen(config.server.port, config.server.ip, function(error) {
+app.listen( config.server.port, config.server.ip, function( error ) {
     if ( error ) {
-        console.log('Unable to listen for connection', error);
+        console.log( 'Unable to listen for connection', error );
         return;
     }
 
-    console.log('Express listening on ' + config.server.ip + ':' + config.server.port);
+    console.log( 'Express listening on ' + config.server.ip + ':' + config.server.port );
 });
